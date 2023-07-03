d4539db51b1f0d7d795c54abd8e8e8e2c3f3fb52
GMANOHAR-1 <122255843+GMANOHAR-1@users.noreply.github.com>
2023-07-03 13:32:29 +0530

    Create findduplicate.cpp

---
+++ findduplicate.cpp
@@ -0,0 +1,21 @@
+ 
+
+int findDuplicate(vector<int> &arr, int n)
+{
+
+     for(int i = 0; i < n; i++) 
+    {
+
+             int index = abs(arr[i]) - 1;
+            
+             arr[index] *= -1;
+            
+             if(arr[index] > 0)
+            {
+                
+                 return abs(arr[i]);
+            }   
+    } 
+    
+    return -1;
+}
