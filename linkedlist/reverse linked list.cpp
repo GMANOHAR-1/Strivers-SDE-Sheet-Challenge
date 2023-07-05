8e1523cbc3815135e491c2d3b906b14b09fa5d2f
GMANOHAR-1 <122255843+GMANOHAR-1@users.noreply.github.com>
2023-07-05 19:44:51 +0530

    Create reverse linked list.cpp

---
+++ linkedlist/reverse linked list.cpp
@@ -0,0 +1,23 @@
+ 
+
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+{
+
+    // Creating node for remembering previous node in Linked List.
+    LinkedListNode<int> *prev = NULL;
+
+    // Creating temporory node.
+    LinkedListNode<int> *current = head;
+
+    while (current != NULL) 
+    {
+        LinkedListNode<int> *nextNode = current->next;
+        current->next = prev;
+        prev = current;
+        current = nextNode;
+
+    }
+
+    // Return reverse Linked List.
+    return prev;
+}
